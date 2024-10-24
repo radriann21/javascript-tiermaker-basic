0345a516ebf39d087fee3e38121f7aae1714b131
radriann21 <adrianrangelargu@gmail.com>
2024-10-24 11:02:06 -0400

    added drag and drop basic logic

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
+/*  helpers  */
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
 
+const inputImages = $('#images')
+const imageContainer = $('#images-container')
+const rows = $$('.level')
+const reader = new FileReader()
+
+let sourceContainer = null
+let currentElement = null
+
+function handleDrag(evt) {
+  currentElement = evt.target
+  sourceContainer = currentElement.parentNode
+  evt.dataTransfer.setData('text/plain', currentElement.src)
+}
+
+function handleDragOver(evt) {
+  evt.preventDefault()
+}
+
+function handleDrop(evt) {
+  evt.preventDefault()
+  const { currentTarget, dataTransfer } = evt
+
+  if (sourceContainer && currentElement) {
+    sourceContainer.removeChild(currentElement)
+  }
+
+  if (currentElement) {
+    const src = dataTransfer.getData('text/plain')
+    const imgElement = document.createElement('img')
+    imgElement.src = src
+    imgElement.draggable = true
+    imgElement.addEventListener('dragstart', handleDrag)
+    currentTarget.appendChild(imgElement)
+  }
+}
+
+function createImg(evt) {
+  const file = evt.target.files[0]
+  reader.readAsDataURL(file)
+
+  reader.onload = (evt) => {
+    const result = evt.target.result
+    const imgElement = document.createElement('img')
+    imgElement.src = result
+    imgElement.draggable = true
+    imgElement.addEventListener('dragstart', handleDrag)
+    imageContainer.appendChild(imgElement)
+  } 
+}
+
+inputImages.addEventListener('change', createImg)
+
+
+rows.forEach(row => {
+  row.addEventListener('dragover', handleDragOver)
+  row.addEventListener('drop', handleDrop)
+})
