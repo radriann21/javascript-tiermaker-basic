90cfb1d64417458f723b1eef8bc1a133f6b1d50c
radriann21 <adrianrangelargu@gmail.com>
2024-10-25 20:55:53 -0400

    added reset and preview logic

--- main.js
+++ main.js
@@ -2,41 +2,11 @@ import { $, $$ } from "./lib/helpers"
 
 const inputImages = $('#images')
 const imageContainer = $('#images-container')
+const resetButton = $('#reset')
 const rows = $$('.level')
 
 let sourceContainer = null
 let currentElement = null
-
-
-function handleDrag(evt) {
-  currentElement = evt.target
-  sourceContainer = currentElement.parentNode
-  evt.dataTransfer.setData('text/plain', currentElement.src)
-}
-
-
-function handleDragOver(evt) {
-  evt.preventDefault()
-}
-
-
-function handleDrop(evt) {
-  evt.preventDefault()
-  const { currentTarget, dataTransfer } = evt
-
-  if (sourceContainer && currentElement) {
-    sourceContainer.removeChild(currentElement)
-  }
-
-  if (currentElement) {
-    const src = dataTransfer.getData('text/plain')
-    const imgElement = document.createElement('img')
-    imgElement.src = src
-    imgElement.draggable = true
-    imgElement.addEventListener('dragstart', handleDrag)
-    currentTarget.appendChild(imgElement)
-  }
-}
 
 
 function createImg(file) {
@@ -64,14 +34,85 @@ function createItems(evt) {
   } else {
     createImg(files[0])
   }
+}
+
+
+function createElement(evt) {
+  const { currentTarget, dataTransfer } = evt
+
+  if (sourceContainer && currentElement) {
+    sourceContainer.removeChild(currentElement)
+  }
+
+  if (currentElement) {
+    const src = dataTransfer.getData('text/plain')
+    const imgElement = document.createElement('img')
+    imgElement.src = src
+    imgElement.addEventListener('dragstart', handleDrag)
+    currentTarget.appendChild(imgElement)
+  }
+}
+
+
+function handleDrag(evt) {
+  currentElement = evt.target
+  sourceContainer = currentElement.parentNode
+  evt.dataTransfer.setData('text/plain', currentElement.src)
+}
+
+
+function handleDragOver(evt) {
+  evt.preventDefault()
+  const { currentTarget } = evt
+  if (sourceContainer === currentTarget) return
   
+  currentTarget.classList.add('drag-over')
+
+  const dragPreview = currentTarget.querySelector('.prev')
+
+  if (currentElement && !dragPreview) {
+    const previewElement = currentElement.cloneNode(true)
+    previewElement.classList.add('prev')
+    currentTarget.appendChild(previewElement)
+  }
+}
+
+
+function handleDrop(evt) {
+  evt.preventDefault()
+
+  const { currentTarget } = evt
+
+  createElement(evt)
+  currentTarget.querySelector('.prev')?.remove()
+  currentTarget.classList.remove('drag-over')
+}
+
+
+function handleDragLeave(evt) {
+  evt.preventDefault()
+
+  const { currentTarget } = evt
+  currentTarget.classList.remove('drag-over')
+  currentTarget.querySelector('.prev')?.remove()
 }
 
 
 inputImages.addEventListener('change', createItems)
 
-
 rows.forEach(row => {
   row.addEventListener('dragover', handleDragOver)
   row.addEventListener('drop', handleDrop)
+  row.addEventListener('dragleave', handleDragLeave)
+})
+
+imageContainer.addEventListener('dragover', handleDragOver)
+imageContainer.addEventListener('drop', handleDrop)
+
+resetButton.addEventListener('click', () => {
+  const elements = $$('.level img')
+  elements.forEach(element => {
+    element.remove()
+    imageContainer.appendChild(element)
+  })
 })
